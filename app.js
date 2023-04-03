61bfd6b440fd9465056d3e1a2302357d0b16f028
Grig <74515267+Grigor-star@users.noreply.github.com>
2023-04-04 00:35:55 +0400

    Add files via upload

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const btns = document.querySelectorAll(".nav-btn");
 const slideVideos = document.querySelectorAll(".video-slide")
+const introInner = document.querySelectorAll(".intro_inner")
 let sliderNav = function(manual){
     btns.forEach((btn) => {
         btn.classList.remove("active")
@@ -9,8 +10,13 @@ let sliderNav = function(manual){
         slide.classList.remove("active")
     })
     
+    introInner.forEach((content) =>{
+        content.classList.remove("active")
+    })
+    
     btns[manual].classList.add("active");
     slideVideos[manual].classList.add("active");
+    introInner[manual].classList.add("active")
 }
 btns.forEach((btn, i) => {
     btn.addEventListener("click", () => {
